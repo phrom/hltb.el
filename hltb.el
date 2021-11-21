1098876051558391dd9963461c250e17c1189fdb
Pedro Henrique Romano <mail@pedroromano.org>
2021-11-21 12:17:06 -0300

    Parse time estimates

--- hltb.el
+++ hltb.el
@@ -30,7 +30,12 @@
   id title img main main+extra completionist)
 
 (defun hltb-parse-time (time)
-  (string-to-number time))
+  (let ((number (string-to-number time))
+        (half (string-match-p "½" time))
+        (hour-p (string-match-p "Hour" time)))
+    (if hour-p
+        (+ (* 60 number) (if half 30 0))
+      number)))
 
 (defun hltb-search (name)
   (let* ((response (hltb-url-retrieve-post "https://howlongtobeat.com/search_results?page=1"
@@ -42,21 +47,24 @@
                                (let* ((a (car (dom-by-tag li 'a)))
                                       (href (dom-attr a 'href))
                                       (img (car (dom-by-tag a 'img)))
-                                      (tidbits (dom-by-class li "^search_list_tidbit center time_100$")))
+                                      (tidbits (dom-by-class li "^search_list_tidbit")))
                                  (make-hltb-entry :id (when (string-match "game\\?id=\\([0-9]+\\)" href)
                                                         (string-to-number (match-string 1 href)))
                                                   :title (dom-attr a 'title)
                                                   :img (concat "https://howlongtobeat.com" (dom-attr img 'src))
-                                                  :main (hltb-parse-time (dom-text (car tidbits)))
-                                                  :main+extra (hltb-parse-time (dom-text (cadr tidbits)))
-                                                  :completionist (hltb-parse-time (dom-text (caddr tidbits))))))
+                                                  :main (hltb-parse-time (dom-text (nth 1 tidbits)))
+                                                  :main+extra (hltb-parse-time (dom-text (nth 3 tidbits)))
+                                                  :completionist (hltb-parse-time (dom-text (nth 5 tidbits))))))
                              (dom-by-tag response 'li))))
     (if (cdr candidates)
         (let ((chosen (completing-read "Game: " (mapcar #'hltb-entry-title candidates))))
           (cl-find-if (lambda (x) (string-equal (hltb-entry-title x) chosen)) candidates))
       (car candidates))))
 
-;(setq result (hltb-search "fallout"))
+;(setq result (hltb-search "Railway Empire"))
+;(setq result (hltb-search "A Bird Story"))
+;(setq result (hltb-search "Stanley Parable Demo"))
+;(setq result (hltb-search "Fallout"))
 ;(hltb-entry-main result)
 
 (provide 'hltb)
