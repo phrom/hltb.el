b4bfd9b13d60bb8d740370ebec4bf09efa4595d0
Pedro Henrique Romano <mail@pedroromano.org>
2021-12-22 16:15:41 -0300

    Split tests into another file

--- hltb.el
+++ hltb.el
@@ -19,7 +19,6 @@
 ;;
 ;;; Code:
 (require 'dom)
-(require 'ert)
 (require 'org)
 (require 'org-attach)
 (require 'org-roam)
@@ -133,45 +132,6 @@
                     :developer developer
                     :publisher publisher
                     :release-date release-date)))
-
-(ert-deftest single-result-test ()
-  (let ((result (hltb-search "Railway Empire")))
-    (should (equal result
-                   (make-hltb-search-entry :title "Railway Empire"
-                                    :img "https://howlongtobeat.com/games/52962_Railway_Empire.jpg"
-                                    :id 52962
-                                    :main 1170
-                                    :main+extra 2790
-                                    :completionist 7140)))))
-
-(ert-deftest multiple-results-test ()
-  (flet ((completing-read (_prompt _candidates) "Fallout 2"))
-    (let ((result (hltb-search "Fallout")))
-      (should (equal result
-                     (make-hltb-search-entry :title "Fallout 2"
-                                             :img "https://howlongtobeat.com/games/250px-PC_Game_Fallout_2.jpg"
-                                             :id 3339
-                                             :main 1830
-                                             :main+extra 2880
-                                             :completionist 4890))))))
-
-(ert-deftest solo-time-test ()
-  (let ((result (hltb-search "Bird Story")))
-    (should (equal result
-                   (make-hltb-search-entry :title "A Bird Story"
-                                    :img "https://howlongtobeat.com/games/ABirdStory_header.jpg"
-                                    :id 22526
-                                    :main 90)))))
-
-(ert-deftest time-in-minutes-test ()
-  (let ((result (hltb-search "Stanley Parable Demo")))
-    (should (equal result
-                   (make-hltb-search-entry :title "The Stanley Parable Demo"
-                                    :img "https://howlongtobeat.com/games/TheStanleyParable.png"
-                                    :id 14118
-                                    :main 28
-                                    :main+extra 38
-                                    :completionist 50)))))
 
 (defun hltb-org-get-link-description (link)
   (if (string-match org-link-bracket-re link)
